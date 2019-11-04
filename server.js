fc6e953495f667901fd400c573cd195ae02d0df9
Abraham, Jakob <jakob.abraham@baloise.ch>
2019-11-04 09:16:28 +0100

    changed port to 9091

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express'),
 app = express(),
 bodyParser = require('body-parser');
-port = process.env.PORT || 3001;
+port = process.env.PORT || 9091;
 
 const mysql = require('mysql');
 
